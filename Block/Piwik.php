9f5518701f00cb9565f209ea0bb2e625c2ffe0b2
Henrik Hedelund <henrik@improove.se>
2015-12-27 13:28:58 +0100

    Added 'trackSiteSearch' to search result page

--- Block/Piwik.php
+++ Block/Piwik.php
@@ -88,11 +88,16 @@ class Piwik extends \Magento\Framework\View\Element\Template
     protected function _prepareTracker()
     {
         $tracker = $this->getTracker();
+
         $this->_eventManager->dispatch(
             'piwik_track_page_view_before',
             ['block' => $this, 'tracker' => $tracker]
         );
-        $tracker->trackPageView();
+
+        if (!$this->getSkipTrackPageView()) {
+            $tracker->trackPageView();
+        }
+
         $this->_eventManager->dispatch(
             'piwik_track_page_view_after',
             ['block' => $this, 'tracker' => $tracker]
---
+++ Observer/SearchResultObserver.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Copyright 2015 Henrik Hedelund
+ *
+ * This file is part of Henhed_Piwik.
+ *
+ * Henhed_Piwik is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Henhed_Piwik is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Henhed_Piwik.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Henhed\Piwik\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Observer for `controller_action_layout_render_before_catalogsearch_result_index'
+ *
+ * @link http://developer.piwik.org/guides/tracking-javascript-guide#internal-search-tracking
+ */
+class SearchResultObserver implements ObserverInterface
+{
+
+    /**
+     * Piwik tracker instance
+     *
+     * @var \Henhed\Piwik\Model\Tracker
+     */
+    protected $_piwikTracker;
+
+    /**
+     * Search query factory
+     *
+     * @var \Magento\Search\Model\QueryFactory $_queryFactory
+     */
+    protected $_queryFactory;
+
+    /**
+     * Current view
+     *
+     * @var \Magento\Framework\App\ViewInterface $_view
+     */
+    protected $_view;
+
+    /**
+     * Constructor
+     *
+     * @param \Henhed\Piwik\Model\Tracker $piwikTracker
+     * @param \Magento\Search\Model\QueryFactory $queryFactory
+     * @param \Magento\Framework\App\ViewInterface $view
+     */
+    public function __construct(
+        \Henhed\Piwik\Model\Tracker $piwikTracker,
+        \Magento\Search\Model\QueryFactory $queryFactory,
+        \Magento\Framework\App\ViewInterface $view
+    ) {
+        $this->_piwikTracker = $piwikTracker;
+        $this->_queryFactory = $queryFactory;
+        $this->_view = $view;
+    }
+
+    /**
+     * Push `trackSiteSearch' to tracker on search result page
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return \Henhed\Piwik\Observer\SearchResultObserver
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $query = $this->_queryFactory->get();
+        $piwikBlock = $this->_view->getLayout()->getBlock('piwik.tracker');
+        /* @var $query \Magento\Search\Model\Query */
+        /* @var $piwikBlock \Henhed\Piwik\Block\Piwik */
+
+        $keyword = $query->getQueryText();
+        $resultsCount = $query->getNumResults();
+
+        if (is_null($resultsCount)) {
+            // If this is a new search query the result count hasn't been saved
+            // yet so we have to fetch it from the search result block instead.
+            $resultBock = $this->_view->getLayout()->getBlock('search.result');
+            /* @var $resultBock \Magento\CatalogSearch\Block\Result */
+            if ($resultBock) {
+                $resultsCount = $resultBock->getResultCount();
+            }
+        }
+
+        if (is_null($resultsCount)) {
+            $this->_piwikTracker->trackSiteSearch($keyword);
+        } else {
+            $this->_piwikTracker->trackSiteSearch(
+                $keyword,
+                false,
+                (int) $resultsCount
+            );
+        }
+
+        if ($piwikBlock) {
+            // Don't push `trackPageView' when `trackSiteSearch' is set
+            $piwikBlock->setSkipTrackPageView(true);
+        }
+
+        return $this;
+    }
+}
