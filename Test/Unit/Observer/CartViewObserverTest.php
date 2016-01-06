269827d91c996e947d70649b3ef7608097cfe699
Henrik Hedelund <henrik@improove.se>
2016-01-06 12:27:40 +0100

    Cart view observer unit test

---
+++ Test/Unit/Observer/CartViewObserverTest.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Copyright 2016 Henrik Hedelund
+ *
+ * This file is part of Henhed_Piwik.
+ *
+ * Henhed_Piwik is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Henhed_Piwik is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Henhed_Piwik.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Henhed\Piwik\Test\Unit\Observer;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
+/**
+ * Test for \Henhed\Piwik\Observer\CartViewObserver
+ *
+ */
+class CartViewObserverTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Cart view observer (test subject) instance
+     *
+     * @var \Henhed\Piwik\Observer\CartViewObserver
+     */
+    protected $_observer;
+
+    /**
+     * Event observer mock object
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject $_eventObserverMock
+     */
+    protected $_eventObserverMock;
+
+    /**
+     * Tracker mock object
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject $_eventObserverMock
+     */
+    protected $_trackerMock;
+
+    /**
+     * Tracker helper mock object
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject $_eventObserverMock
+     */
+    protected $_trackerHelperMock;
+
+    /**
+     * Checkout session mock object
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject $_eventObserverMock
+     */
+    protected $_checkoutSessionMock;
+
+    /**
+     * Quote mock object
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject $_eventObserverMock
+     */
+    protected $_quoteMock;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $className = 'Henhed\Piwik\Observer\CartViewObserver';
+        $objectManager = new ObjectManager($this);
+        $arguments = $objectManager->getConstructArguments($className);
+        $this->_observer = $objectManager->getObject($className, $arguments);
+        $this->_trackerMock = $arguments['piwikTracker'];
+        $this->_trackerHelperMock = $arguments['trackerHelper'];
+        $this->_checkoutSessionMock = $arguments['checkoutSession'];
+        $this->_eventObserverMock = $this->getMock(
+            'Magento\Framework\Event\Observer', [], [], '', false
+        );
+        $this->_quoteMock = $this->getMock(
+            'Magento\Quote\Model\Quote', [], [], '', false
+        );
+    }
+
+    /**
+     * Test for \Henhed\Piwik\Observer\CartViewObserver::execute
+     *
+     * @return void
+     */
+    public function testExecute()
+    {
+        // Provide quote mock access from checkout session mock
+        $this->_checkoutSessionMock
+            ->expects($this->any())
+            ->method('getQuote')
+            ->willReturn($this->_quoteMock);
+
+        // Make sure the tracker helpers `addQuote' is called exactly once with
+        // provided quote and tracker. Actual behavior of `addQuote' is covered
+        // by \Henhed\Piwik\Test\Unit\Helper\TrackerTest.
+        $this->_trackerHelperMock
+            ->expects($this->once())
+            ->method('addQuote')
+            ->with($this->_quoteMock, $this->_trackerMock)
+            ->willReturn($this->_trackerHelperMock);
+
+        // Assert that `execute' returns $this
+        $this->assertSame(
+            $this->_observer,
+            $this->_observer->execute($this->_eventObserverMock)
+        );
+    }
+}
